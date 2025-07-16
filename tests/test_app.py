3beff1afbfd9c0d1f43a75a48a9643a567dcde0e
katehjd21 <katedrew2103@hotmail.co.uk>
2025-07-16 09:59:56 +0100

    added files

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,17 @@
-from app import app
 
-def test_
+
+def test_homepage(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Kate Drew" in response.data
+    assert b"Believe you can and you're halfway there." in response.data
+    assert b"Theodore Roosevelt" in response.data
+
+def test_colour_picker_route(client):
+    response = client.get("/colour-picker")
+    assert response.status_code == 200
+    assert b"Colour Picker" in response.data
+
+def test_non_existent_route(client):
+    response = client.get('/non-existent')
+    assert response.status_code == 404
