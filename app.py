11e9ff050c13eda5100c276959c8e11bfef4fef7
katehjd21 <katedrew2103@hotmail.co.uk>
2025-07-17 15:56:56 +0100

    updated pytest config

--- app.py
+++ app.py
@@ -10,5 +10,7 @@ def colourPicker():
    return render_template('colour-picker.html')
 
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from app import app as flask_app
 
 @pytest.fixture
 def app():
+    flask_app.config['TESTING'] = True
     return flask_app
 
 
