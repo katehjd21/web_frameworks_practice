e09d60828b08ce4ffb53048d36144fe6d88657c3
katehjd21 <katedrew2103@hotmail.co.uk>
2025-07-17 15:50:18 +0100

    created docker image

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
    return render_template('index.html')
-# if __name__ == '__main__':
-#    app.run(debug=True)
 
 @app.route('/colour-picker')
 def colourPicker():
    return render_template('colour-picker.html')
-# if __name__ == '__main__':
-#    app.run(debug=True)
 
+
+app.run(host='0.0.0.0', port=5000, debug=True)
 
